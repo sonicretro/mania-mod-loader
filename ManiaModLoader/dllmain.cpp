18ddd5c3c5f7d643602d56a35100b5d145f7f7bf
Rubberduckycooly <rubberduckycooly@outlook.com>
2020-12-08 16:33:40 +1100

    slight fix

--- ManiaModLoader/dllmain.cpp
+++ ManiaModLoader/dllmain.cpp
@@ -638,6 +638,22 @@ struct SKUInfo {
 	DWORD Region;
 };
 
+struct ScreenInfo {
+	WORD FrameBuffer[0x4B000];
+	DWORD XPos;
+	DWORD YPos;
+	DWORD Width;
+	DWORD Height;
+	DWORD ScreenCenterX;
+	DWORD ScreenCenterY;
+	DWORD Pitch;
+	DWORD UnknownA;
+	DWORD UnknownB;
+	DWORD ScreenWidth2;
+	DWORD Height2;
+	DWORD Height3;
+};
+
 struct GameInfo {
 	void* FunctionPtrs; //Array of function ptrs
 	void* UserdataPtrs; //Array of userdata func ptrs
@@ -651,14 +667,16 @@ struct GameInfo {
 	void* dword24;
 	void* dword28;
 	void* dword2C;
-	void* ScreenInfo;
+	ScreenInfo* ScreenInfo;
 };
 
 SKUInfo skuInfo;
+char* GameName = "";
 DataPointer(HMODULE, GameDLLModule, 0x6ECA10);
 ThiscallFunctionPointer(int, SetupObjects, (GameInfo* GameInfo), 0x1A6E20);
 static int LinkGameLogic(GameInfo* GameInfo) {
 	GameInfo->CurrentSKU = &skuInfo;
+	//GameName = GameInfo->GameName;
 	int result = SetupObjects(GameInfo);
 	RaiseEvents(modLinkEvents); 
 	GameDLLModule = NULL; // Game will try freeing it otherwise
@@ -666,7 +684,7 @@ static int LinkGameLogic(GameInfo* GameInfo) {
 };
 
 HMODULE GetCurrentModule()
-{ // NB: XP+ solution!
+{
 	HMODULE hModule = NULL;
 	GetModuleHandleEx(
 		GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
